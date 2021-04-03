751173ffef880752fbc71e9d06edc9790a48a680
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-04-03 13:29:05 +0900

    profileモデル作成、showのcss実装。 デーアベース紐付け

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'cards#index'
+
+  resources :cards do
+  end
+
+  resource :profile, only: [ :show, :edit, :update]
 end
