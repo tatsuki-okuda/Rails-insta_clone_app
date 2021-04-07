c0400ed648c48363d7db0dfb416a91c411a6e096
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-04-07 12:56:37 +0900

    投稿のテキストと画像の保存まで。　画像入れたらプレビュー出したい。。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_013253) do
+ActiveRecord::Schema.define(version: 2021_04_07_022632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2021_04_05_013253) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
