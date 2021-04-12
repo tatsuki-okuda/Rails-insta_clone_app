9c58e3388de2c62259fddf52a9a757284b085317
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-04-12 19:24:30 +0900

    avatarモデルを追加して検証をする

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,10 +11,13 @@ class ProfilesController < ApplicationController
   end
 
   def create
+    
     # current_userに紐づくprofileの空箱を作る。データはavateだけ
+
+    
     @profile = current_user.profile.build(profile_params)
-
-
+    # @profile = current_user.build_profile(profile_params)
+    
     @profile.save!
     render json: @profile
   end
@@ -23,6 +26,7 @@ class ProfilesController < ApplicationController
   private
   def profile_params
     params.require(:profile).permit(:avatar)
+    # params.permit(:avatar)
     # {profile: {avatar: 'aaaaa'}}
   end
   
