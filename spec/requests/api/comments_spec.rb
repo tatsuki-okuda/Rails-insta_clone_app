784d4bc1e35e95f887fe2a244bdf46e668633e9b
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-05-14 21:15:07 +0900

    view　テスト

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -7,10 +7,21 @@ RSpec.describe 'Api::Comments', type: :request do
 
   describe 'GET /comments' do
     it 'status 200' do
-      get json_card_comments_path(card_id: card.id)
+      get json_card_comments_path(card_id: card.id)  
       
+      # *****************************
+      # status 500で返ってくる
+      # *****************************
       expect(response).to have_http_status(200)
+
+      body = JSON.parse(response.body)
+      expect(body.length).to eq 3
+      expect(body[0]['content']).to eq comments.first.content
+      expect(body[1]['content']).to eq comments.second.content
+      expect(body[2]['content']).to eq comments.third.content
     end
   end
+
+  
 
 end
